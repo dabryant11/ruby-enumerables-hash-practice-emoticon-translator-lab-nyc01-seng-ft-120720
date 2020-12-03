7c6e494878cb5f2f14fca0e3601db155bfef6053
darien bryant <darienbryant11@gmail.com>
2020-12-03 20:24:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -50,6 +50,7 @@ def get_english_meaning(file_path,emoticon)
 
     emoticons.each do |key,value|
         value.each do |key2, value2|
+          binding.pry 
           if emoticon == value2
             return value[:english]
           end 
